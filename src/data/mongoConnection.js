9ee206247024390be4baffb2bbff73ad20eae599
Chirag <chiragk927@gmail.com>
2021-10-04 04:14:52 -0400

    connected to atlas, modified env variables, updated dependencies

--- src/data/mongoConnection.js
+++ src/data/mongoConnection.js
@@ -1,15 +1,24 @@
+require('dotenv').config()
 const MongoClient = require("mongodb").MongoClient;
-const settings = require("./settings");
-const mongoConfig = settings.mongoConfig;
+// const settings = require("./settings");
+// const mongoConfig = settings.mongoConfig;
 
+
+console.log(process.env.SERVER_URL, '\n\n\n')
 let _connection = undefined;
 let _db = undefined;
 
 module.exports = async () => {
   if (!_connection) {
-    
-    _connection = await MongoClient.connect(mongoConfig.serverUrl, {useNewUrlParser: true, useUnifiedTopology:true});
-    _db = await _connection.db(mongoConfig.database);
+    try{
+    // _connection = await MongoClient.connect(mongoConfig.serverUrl, {useNewUrlParser: true, useUnifiedTopology:true});
+    // _db = await _connection.db(mongoConfig.database);
+    _connection = await MongoClient.connect(process.env.SERVER_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    _db = await _connection.db(process.env.DATABASE);
+    }
+    catch(Exception) {
+      console.log(Exception,'\n\n\n')
+    }
   }
 
   return _db;
