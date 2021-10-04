8e1e5aef57bb7b05a135e065a7294fcc7bdea93e
Chirag <chiragk927@gmail.com>
2021-10-04 04:15:36 -0400

    refactored

--- src/data/mongoConnection.js
+++ src/data/mongoConnection.js
@@ -1,18 +1,12 @@
 require('dotenv').config()
 const MongoClient = require("mongodb").MongoClient;
-// const settings = require("./settings");
-// const mongoConfig = settings.mongoConfig;
 
-
-console.log(process.env.SERVER_URL, '\n\n\n')
 let _connection = undefined;
 let _db = undefined;
 
 module.exports = async () => {
   if (!_connection) {
     try{
-    // _connection = await MongoClient.connect(mongoConfig.serverUrl, {useNewUrlParser: true, useUnifiedTopology:true});
-    // _db = await _connection.db(mongoConfig.database);
     _connection = await MongoClient.connect(process.env.SERVER_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     _db = await _connection.db(process.env.DATABASE);
     }
